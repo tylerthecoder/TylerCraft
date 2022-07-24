267708b11f0e06792ec169a4fd3ad37ed152cc74
Tyler Tracy <tylertracy1999@gmail.com>
2022-07-24 14:41:18 -0500

    Added dev command to run server

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,9 +3,10 @@ import TylerCraftApp from "./app";
 import * as wSocket from "ws";
 import { MongoClient } from "mongodb";
 import cors from "cors";
+import path from "path";
 
 export const PORT = process.env.PORT ?? 3000;
-export const DB_URL = process.env.DB_URL as string;
+export const DB_URL = process.env.DB_URL;
 
 if (!DB_URL) {
   throw new Error("DB_URL not defined");
@@ -17,7 +18,11 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ limit: "50mb" }));
 
-app.use(express.static("dist/public"));
+
+// Serve the web client
+const webClientPath = path.resolve(__dirname, "../../web/client/dist");
+console.log("Serving web client. ath", webClientPath)
+app.use(express.static(webClientPath));
 
 TylerCraftApp.addRoutes(app);
 
