77b6ace1f71ce9cfda5145117260f8030fd92690
Tyler Tracy <tylertracy1999@gmail.com>
2024-03-31 02:09:27 -0500

    More script craziness

--- scripts/prod-start.sh
+++ scripts/prod-start.sh
@@ -9,9 +9,9 @@ set -x
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+nvm install 20
 nvm use 20
 echo $(which node)
-
 
 yarn
 
